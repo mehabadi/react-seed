6011ba9bce5e2a23a725021184f55e75baafa691
mehabadi <m.mehabadi@gmail.com>
2019-01-13 10:53:21 +0330

    connected to mongodb

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+    mongoURI: 'mongodb://expense_user:Mhsen12sDD@ds251804.mlab.com:51804/expenses'
+}
---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+// connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log('MongoDB Connected');
+  })
+  .catch(err => {
+    console.log(err);
+    console.log('MongoDB Not Connected');
+});
+
+//
+// ─── ROUTES ─────────────────────────────────────────────────────────────────────
+//
+app.get('/', (req, res) => {
+  res.send("Hello World");
+});
+
+//
+// ─── RUN SERVER ─────────────────────────────────────────────────────────────────
+//
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
