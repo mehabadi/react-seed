5ea479e2aa9eae2d79fefa05da689306578fa183
mehabadi <m.mehabadi@gmail.com>
2019-01-13 14:41:01 +0330

    add path

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const path = require('path');
 
 // DB Config
 const db = require('./config/keys').mongoURI;
